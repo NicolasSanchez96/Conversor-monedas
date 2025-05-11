993c1f8aaa6b37aadf51dbb11191693620b46f66
NicolasSanchez96 <ngcircus@gmail.com>
2025-05-10 21:38:48 -0300

    Conversiones

--- src/Principal.java
+++ src/Principal.java
@@ -2,10 +2,35 @@ import java.util.Scanner;
 
 public class Principal {
 
+
+    public static double convertirUSDtoARS(double montoUSD) {
+        return montoUSD * ExchangeRate.tasaARS;
+    }
+
+    public static double convertirARStoUSD(double montoARS) {
+        return montoARS / ExchangeRate.tasaARS;
+    }
+
+    public static double convertirUSDtoBRL(double montoUSD) {
+        return montoUSD * ExchangeRate.tasaBRL;
+    }
+
+    public static double convertirBRLtoUSD(double montoBRL) {
+        return montoBRL / ExchangeRate.tasaBRL;
+    }
+
+    public static double convertirUSDtoCOP(double montoUSD) {
+        return montoUSD * ExchangeRate.tasaCOP;
+    }
+
+    public static double convertirCOPtoUSD(double montoCOP) {
+        return montoCOP / ExchangeRate.tasaCOP;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        // Cargar tasas desde la API
         ExchangeRate.getExchangeRates();
 
         int opcion;
@@ -27,42 +52,42 @@ public class Principal {
                 case 1:
                     System.out.print("Ingrese cantidad en USD: ");
                     double usd1 = scanner.nextDouble();
-                    double ars = usd1 * ExchangeRate.tasaARS;
+                    double ars = convertirUSDtoARS(usd1);
                     System.out.println("Equivalente en ARS: " + ars);
                     break;
 
                 case 2:
                     System.out.print("Ingrese cantidad en ARS: ");
                     double ars2 = scanner.nextDouble();
-                    double usd2 = ars2 / ExchangeRate.tasaARS;
+                    double usd2 = convertirARStoUSD(ars2);
                     System.out.println("Equivalente en USD: " + usd2);
                     break;
 
                 case 3:
                     System.out.print("Ingrese cantidad en USD: ");
                     double usd3 = scanner.nextDouble();
-                    double brl = usd3 * ExchangeRate.tasaBRL;
+                    double brl = convertirUSDtoBRL(usd3);
                     System.out.println("Equivalente en BRL: " + brl);
                     break;
 
                 case 4:
                     System.out.print("Ingrese cantidad en BRL: ");
                     double brl2 = scanner.nextDouble();
-                    double usd4 = brl2 / ExchangeRate.tasaBRL;
+                    double usd4 = convertirBRLtoUSD(brl2);
                     System.out.println("Equivalente en USD: " + usd4);
                     break;
 
                 case 5:
                     System.out.print("Ingrese cantidad en USD: ");
                     double usd5 = scanner.nextDouble();
-                    double cop = usd5 * ExchangeRate.tasaCOP;
+                    double cop = convertirUSDtoCOP(usd5);
                     System.out.println("Equivalente en COP: " + cop);
                     break;
 
                 case 6:
                     System.out.print("Ingrese cantidad en COP: ");
                     double cop2 = scanner.nextDouble();
-                    double usd6 = cop2 / ExchangeRate.tasaCOP;
+                    double usd6 = convertirCOPtoUSD(cop2);
                     System.out.println("Equivalente en USD: " + usd6);
                     break;
 
